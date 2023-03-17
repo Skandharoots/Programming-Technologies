75c6c6aacb90135b369382f1085f9b0eefaf05bf
The Q <matkubiak888@gmail.com>
2023-03-17 14:00:49 +0100

    Added tests for Calculator class

--- SampleProgram/LibraryTest/UnitTest1.cs
+++ SampleProgram/LibraryTest/UnitTest1.cs
@@ -1,11 +1,45 @@
-namespace LibraryTest
-{
+using Library;
+using System.Linq.Expressions;
+
+namespace LibraryTest {
     [TestClass]
-    public class UnitTest1
-    {
+    public class CalculatorTest {
+
+        Calculator calc;
+        int a, b;
+
+        [TestInitialize] public void TestInitialize() {
+            calc = new Calculator();
+            a = 2;
+            b = 3;
+        }
+
+        [TestMethod] public void AddTest() {
+            Assert.AreEqual(calc.Add(a,b), 5, "Lol not work");
+        }
+
+        [TestMethod] public void SubtractTest() {
+            Assert.AreEqual(calc.Subtract(a, b), -1);
+        }
+
+        [TestMethod] public void MultiplyTest() {
+            Assert.AreEqual(calc.Multiply(a, b), 6);
+        }
+
+        [TestMethod] public void DivideTest() {
+            Assert.AreEqual(calc.Divide(a, b), 0);
+        }
+
         [TestMethod]
-        public void TestMethod1()
-        {
+        [ExpectedException(typeof(DivideByZeroException), "Dividing a non-zero by zero doesn't raise exception.")]
+        public void DivideNumberByZeroTest() {
+            calc.Divide(a, 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException), "Dividing zero by zero doesn't raise exception.")]
+        public void DivideZeroByZeroTest() {
+            calc.Divide(0, 0);
         }
     }
 }
