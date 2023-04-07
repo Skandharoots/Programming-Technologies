2bd462dfb51790568baba91962a8edcf5759ab4d
Marek Kopania <234760@edu.p.lodz.pl>
2023-04-07 21:50:44 +0200

    Added tests for logic layer

--- SampleProgram/LibraryTest/LibraryUnitTest.cs
+++ SampleProgram/LibraryTest/LibraryUnitTest.cs
@@ -1,26 +1,90 @@
+using NUnit.Framework;
 using RecordStore;
 
-namespace TestLibrary;
-
-public class Tests
+namespace TestLibrary
 {
-    /* [SetUp]
-     public void Setup()
-     {
-     }
- */
-    [Test]
-    public void Test1()
+    public class Tests
     {
-        Record record = new Record("Nevermind", "Kurt Cobain");
-        string actual = record.Title;
-        Console.WriteLine(record.ToString());
-        Assert.AreEqual("Nevermind", actual);
-        RecordStatus rstatus = new RecordStatus(record, DateTime.Today);
-        DateTime actualTime = DateTime.Today;
-        Assert.AreEqual(actualTime, rstatus.DateOfPurchase);
-        DateTime rentalDate = new DateTime(2022, 05, 23);
-        Event eventOne = new Event(record, new Client("Marek", "Kopania"), rentalDate, DateTime.Now);
-        Console.WriteLine(eventOne.ToString());
+        //LogicTests
+
+        [Test]
+        public void FillRandom() 
+        {
+            DataContext cont = new DataContext();
+            FillRandom random = new FillRandom();
+            random.Fill(cont);
+            int clients = cont.clients.Count;
+            Assert.IsNotNull(clients);
+            int records = cont.records.Count;
+            Assert.IsNotNull(records);
+            int events = cont.events.Count;
+            Assert.IsNotNull(events);
+            int recordStatuses = cont.recordStatuses.Count;
+            Assert.IsNotNull(recordStatuses);
+        }
+
+        [Test]
+        public void FillRandomClients()
+        {
+            DataContext cont = new DataContext();
+            FillRandom random = new FillRandom();
+            int expAmount = 30;
+            random.FillClients(cont);
+            int actual = cont.clients.Count;
+            Assert.AreEqual(actual, expAmount);
+        }
+
+        [Test]
+        public void FillRandomRecords()
+        {
+            DataContext cont = new DataContext();
+            FillRandom random = new FillRandom();
+            int expAmount = 10;
+            random.FillRecords(cont);
+            int actual = cont.records.Count;
+            Assert.AreEqual(actual, expAmount);
+        }
+
+        [Test]
+        public void FillRandomEvents()
+        {
+            DataContext cont = new DataContext();
+            FillRandom random = new FillRandom();
+            int expAmount = 30;
+            random.FillClients(cont);
+            random.FillRecords(cont);
+            random.FillRecordStatuses(cont);
+            random.FillEvents(cont);
+            int actual = cont.events.Count;
+            Assert.AreEqual(actual, expAmount);
+        }
+
+        [Test]
+        public void FillRandomRecordStatuses()
+        {
+            DataContext cont = new DataContext();
+            FillRandom random = new FillRandom();
+            int expAmount = 10;
+            random.FillRecords(cont);
+            random.FillRecordStatuses(cont);
+            int actual = cont.recordStatuses.Count;
+            Assert.AreEqual(actual, expAmount);
+        }
+
+
+        [Test]
+        public void Test1()
+        {
+            Record record = new Record("Nevermind", "Kurt Cobain");
+            string actual = record.Title;
+            Console.WriteLine(record.ToString());
+            Assert.AreEqual("Nevermind", actual);
+            RecordStatus rstatus = new RecordStatus(record, DateTime.Today);
+            DateTime actualTime = DateTime.Today;
+            Assert.AreEqual(actualTime, rstatus.DateOfPurchase);
+            DateTime rentalDate = new DateTime(2022, 05, 23);
+            Event eventOne = new Event(record, new Client("Marek", "Kopania"), rentalDate, DateTime.Now);
+            Console.WriteLine(eventOne.ToString());
+        }
     }
 }
