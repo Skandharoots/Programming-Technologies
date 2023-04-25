6ce016c8d424c943c8d8718dd69004b1f1c76d0d
Marek Kopania <234760@edu.p.lodz.pl>
2023-04-25 17:07:54 +0200

    update: tests for random fill

---
+++ SampleProgram/LibraryTest/FillRandomValuesTest.cs
@@ -0,0 +1,26 @@
+﻿using Data.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibrary
+{
+    internal class FillRandomValuesTest
+    {
+        [Test]
+
+        public void testConstFill()
+        {
+            var datarepo = IDataRepository.CreateRepo(new FillRandomValues());
+            Assert.AreEqual(30, datarepo.GetAllClients().Count());
+            Assert.AreNotEqual(8, datarepo.GetAllClients().Count());
+            Assert.AreEqual(10, datarepo.GetAllRecords().Count());
+            Assert.AreNotEqual(7, datarepo.GetAllRecords().Count());
+            Assert.AreEqual(10, datarepo.GetAllRecordStatus().Count());
+            Assert.AreNotEqual(6, datarepo.GetAllRecordStatus().Count());
+            Assert.AreEqual(30, datarepo.GetAllEvents().Count());
+        }
+    }
+}
