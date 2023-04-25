3ab93f2b2bb384f444a3cf729ff24ee8eee121f7
Marek Kopania <234760@edu.p.lodz.pl>
2023-04-25 16:48:22 +0200

    update: added test for constant fill

---
+++ SampleProgram/LibraryTest/FillConstantValues.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.API;
+using Data.Implementation;
+using Logic.Implementation;
+
+namespace TestLibrary
+{
+    internal class FillConstantValues : IDataGeneration
+    {
+        public override void Fill(IDataRepository dataRepo)
+        {
+            dataRepo.AddClient(new Client("Marek", "Kopania"));
+            dataRepo.AddClient(new Client("Mateusz", "Kubiak"));
+            dataRepo.AddClient(new Client("Grzegoz", "Brzeczyszczykiewicz"));
+            dataRepo.AddClient(new Client("Patryk", "Wazny"));
+            dataRepo.AddClient(new Client("Karol", "Bukowski"));
+
+            dataRepo.AddRecord(new Record(0, "Nirvana", "Nevermind"));
+            dataRepo.AddRecord(new Record(1, "Nirvan", "Nevermin"));
+            dataRepo.AddRecord(new Record(2, "Nirva", "Nevermi"));
+            dataRepo.AddRecord(new Record(3, "Nirv", "Neverm"));
+            dataRepo.AddRecord(new Record(4, "Nir", "Never"));
+
+            dataRepo.AddRecordStatus(new RecordStatus(dataRepo.GetRecord(0), DateTime.Now));
+            dataRepo.AddRecordStatus(new RecordStatus(dataRepo.GetRecord(1), DateTime.Now));
+            dataRepo.AddRecordStatus(new RecordStatus(dataRepo.GetRecord(2), DateTime.Now));
+
+            dataRepo.AddEvent(new Event(dataRepo.GetClient(0), dataRepo.GetRecordStatus(0), DateTime.Now, DateTime.Now.AddHours(3)));
+            dataRepo.AddEvent(new Event(dataRepo.GetClient(3), dataRepo.GetRecordStatus(1), DateTime.Now, DateTime.Now.AddHours(3)));
+            dataRepo.AddEvent(new Event(dataRepo.GetClient(2), dataRepo.GetRecordStatus(2), DateTime.Now, DateTime.Now.AddHours(3)));
+
+        }
+    }
+}
---
+++ SampleProgram/LibraryTest/FillConstantValuesTest.cs
@@ -0,0 +1,25 @@
+﻿using Data.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibrary
+{
+    public class FillConstantValuesTest
+    {
+        [Test]
+
+        public void testConstFill()
+        {
+            var datarepo = IDataRepository.CreateRepo(new FillConstantValues());
+            Assert.AreEqual("Marek", datarepo.GetClient(0).Name);
+            Assert.AreNotEqual(8, datarepo.GetAllClients().Count());
+            Assert.AreEqual("Nevermind", datarepo.GetRecord(0).Author);
+            Assert.AreNotEqual(7, datarepo.GetAllRecords().Count());
+            Assert.AreEqual(datarepo.GetRecord(0), datarepo.GetRecordStatus(0).record);
+            Assert.AreNotEqual(6, datarepo.GetAllRecordStatus().Count());
+        }
+    }
+}
