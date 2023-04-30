b2ece87a7ac5c5256e62d7baa14f84bd82351964
The Q <matkubiak888@gmail.com>
2023-04-30 18:49:26 +0200

    move: DTOs and CRUDs moved to Implementation dir

--- Task_2/Service/Implementation/EventCRUD.cs
+++ Task_2/Service/Implementation/EventCRUD.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Data.API;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
--- Task_2/Service/Implementation/RecordCRUD.cs
+++ Task_2/Service/Implementation/RecordCRUD.cs
@@ -8,5 +8,6 @@ namespace Service.CRUD
 {
     public class RecordCRUD
     {
+
     }
 }
