134a03c199a65ba9229d48e50ed51942eed44248
The Q <matkubiak888@gmail.com>
2023-05-02 03:36:35 +0200

    add: remaining CRUD implementations

--- Task_2/Service/Implementation/RecordCRUD.cs
+++ Task_2/Service/Implementation/RecordCRUD.cs
@@ -1,13 +1,63 @@
-﻿using System;
+﻿using Data.API;
+using Service.DTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Service.CRUD
-{
-    public class RecordCRUD
-    {
+namespace Service.CRUD {
 
+    public class RecordCRUD {
+
+        private DataLayerAPI dataLayer;
+
+        public RecordCRUD() {
+            dataLayer = DataLayerAPI.CreateLayer();
+        }
+
+        public RecordCRUD(DataLayerAPI dataLayer) {
+            this.dataLayer = dataLayer;
+        }
+
+        private RecordDTO Map(IRecord record) {
+            if (record == null)
+                return null;
+            return new RecordDTO(record.Id, record.Author, record.Title);
+        }
+
+
+        public void AddRecord(string author, string title) {
+            dataLayer.AddRecord(author, title);
+        }
+
+        public void DeleteRecord(int id) {
+            dataLayer.DeleteRecord(id);
+        }
+
+
+        public void UpdateAuthor(int id, string author) {
+            dataLayer.UpdateRecordAuthor(id, author);
+        }
+
+        public void UpdateTitle(int id, string title) {
+            dataLayer.UpdateRecordTitle(id, title);
+        }
+
+
+        public RecordDTO GetRecord(int id) {
+            return Map(dataLayer.GetRecord(id));
+        }
+
+        public IEnumerable<RecordDTO> GetAllRecords() {
+
+            var records = dataLayer.GetAllRecords();
+            var result = new List<RecordDTO>();
+
+            foreach (var record in records)
+                result.Add(Map(record));
+
+            return result;
+        }
     }
 }
