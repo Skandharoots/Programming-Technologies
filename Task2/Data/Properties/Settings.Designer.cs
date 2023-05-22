5db35d176286bda918eb2480f4b24dc0bbeb081d
Marek Kopania <234760@edu.p.lodz.pl>
2023-05-22 13:44:24 +0200

    fix: fixed settings

--- Task2/Data/Properties/Settings.Designer.cs
+++ Task2/Data/Properties/Settings.Designer.cs
@@ -27,7 +27,7 @@ namespace Data.Properties {
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
         [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\DataBase\\DB.m" +
-            "df;Integrated Security=True;Connect Timeout=30")]
+            "df;Integrated Security=True;Connect Timeout=30;")]
         public string DBConnectionString {
             get {
                 return ((string)(this["DBConnectionString"]));
